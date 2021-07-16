bae8e8fbd3456b37da18c3a57973f2edc91cdc30
isabel.llanes <isabel.llanes@thougthworks.com>
2021-07-16 19:18:41 +0200

    Use setEntries to clean the state

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React, {useState} from "react";
 
 
 function App() {
-    const[entries, setEntries] = useState([
+    const [entries, setEntries] = useState([
         {title: "title 1", content: "content 1"},
         {title: "title 2", content: "content 2"},
     ])
+    const clear = () => {
+        setEntries([])
+    }
     return (
-        <ul>
-            {entries.map(item => <li>{item.title} - {item.content}</li>)}
-        </ul>
+        <div>
+            <ul>
+                {entries.map(item => <li>{item.title} - {item.content}</li>)}
+            </ul>
+            <button onClick={clear}>Clear</button>
+        </div>
     );
 }
 
